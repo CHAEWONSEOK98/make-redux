8c1e07ee92850f01f28d956cb7386ade73b5279e
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-17 16:38:48 +0900

    redux-tutorial-subscribe

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,7 +4,9 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+//reducer
 const countModifier = (count = 0, action) => {
+  console.log(count, action);
   if (action.type === "ADD") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -14,12 +16,23 @@ const countModifier = (count = 0, action) => {
   }
 };
 
+//store
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+//change check
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+//subscribe
+countStore.subscribe(onChange);
+
+//event
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: "MINUS" });
+});
