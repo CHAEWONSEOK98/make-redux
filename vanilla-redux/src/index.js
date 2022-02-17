62a8923115eda06f3cd98af82988c897401a8fb8
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-17 16:49:32 +0900

    Refactoring

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,15 +4,18 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 //reducer
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -29,10 +32,10 @@ countStore.subscribe(onChange);
 
 //event
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 });
