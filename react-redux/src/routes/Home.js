04b706504a6ba509f2db6dcfd67656690ec887f2
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-20 03:22:14 +0900

    react-redux deleting to do list

--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
 function Home({ toDos, addToDo }) {
-  console.log(addToDo);
   const [text, setText] = useState("");
 
   const onChange = (e) => {
@@ -22,7 +22,11 @@ function Home({ toDos, addToDo }) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul>{JSON.stringify(toDos)}</ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
