9d8d4fa26b802e847a9387ae936afe6f84e32a85
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-20 22:14:22 +0900

    react-redux detail

--- react-redux/src/routes/Detail.js
+++ react-redux/src/routes/Detail.js
@@ -1,1 +1,22 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDo }) {
+  const myId = useParams().id;
+  const toDoText = toDo.find((toDo) => toDo.id === parseInt(myId));
+  console.log(toDoText);
+
+  return (
+    <>
+      {toDoText?.text} <br />
+      Created at: {toDo?.id}
+    </>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDo: state };
+}
+
+export default connect(mapStateToProps)(Detail);
